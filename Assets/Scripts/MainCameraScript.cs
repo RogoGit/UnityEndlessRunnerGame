a01bd240fa7ecfa1ea1b3bf35dc72426fcd7a6bf
Nikita Rogalenko <nikitalr@mail.ru>
2020-11-07 16:00:17 +0300

    player and camera movement

---
+++ Assets/Scripts/MainCameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour
+{
+
+    private Transform lookAt;
+    private Vector3 camOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // searching fpr player object by tag
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        camOffset = transform.position - lookAt.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = lookAt.position + camOffset;
+    }
+}
---
+++ Assets/Scripts/PlayerMovings.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovings : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 moveVector;
+    private float characterSpeed = 5.0f;
+    private float verticalSpeed = 0.0f;
+    private float gravity = 12.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = Vector3.zero; // reseting direction every frame
+
+        // check if player is on the floor
+        if (controller.isGrounded) 
+        {
+            verticalSpeed = -0.5f;
+        } 
+        else
+        {
+            verticalSpeed -= gravity * Time.deltaTime;
+        }
+
+        // Left-right (x axis)
+        moveVector.x = Input.GetAxisRaw("Horizontal") * characterSpeed;
+
+        // Up-down (y axis)
+        moveVector.y = verticalSpeed;
+
+        // Forward-backward (z axis)
+        moveVector.z = characterSpeed;
+
+        controller.Move(moveVector * Time.deltaTime);
+    }
+}
